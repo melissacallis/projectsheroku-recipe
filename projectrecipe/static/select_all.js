2c89acc961b79909c3e2a6abb8c2cf7926f04fc3
Melissa Yvette Cavazos <109395704+melissacallis@users.noreply.github.com>
2023-09-17 01:20:12 +0000

    removed second style folder in root directory.

--- projectrecipe/static/select_all.js
+++
@@ -1,8 +0,0 @@
-// select_all.js
-$("#selectall").change(function() {
-    if ($(this).is(":checked")) {
-        $(".checkboxSelection").prop('checked', true);
-    } else {
-        $(".checkboxSelection").prop('checked', false);
-    }
-});
